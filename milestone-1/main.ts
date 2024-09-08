15622fdab816ded583207fc0ad7f74e7c7228163
AmaanIqbal0011 <166907241+AmaanIqbal0011@users.noreply.github.com>
2024-09-08 13:24:18 +0500

    add button css
    CSS for button element

--- milestone-1/main.ts
+++ milestone-1/main.ts
@@ -2,7 +2,7 @@ const button = document.getElementById("toggle-skills") as HTMLButtonElement;
 
 const changecolor = ()  => {
 document.body.style.backgroundColor = 'grey'
-document.body.style.backgroundColor = '#f4f4f4'
+
 }
 
 button.addEventListener('click',changecolor)
